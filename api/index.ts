b6ec9f7d054203e55973403bba5bcbfe30c9b451
Ricardo-we <totgamer75@gmail.com>
2022-10-30 22:38:53 -0600

    removed auto sync

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,3 @@
-import { getAppModels, syncModelList } from "./src/utils/Db/db.utils";
-
-import DbRepository from "./src/services/db/DbRepository";
 import appConfig from "./src/config/index";
 import { appUseRoutes } from "./src/utils/route.utils";
 import express from "express";
@@ -9,16 +6,11 @@ const app = express();
 
 // GLOBAL MIDDLEWARES
 app.use(express.json());
-const conn = DbRepository.getConnection("mssql");
 
 async function main() {
     try {
         await appUseRoutes(app);
-        const appModels = await getAppModels(appConfig.apps); 
-        console.log(appModels);
-        await syncModelList(appModels);
-
-        await conn.sync();
+        
         app.listen(appConfig.APP_PORT, () => {
             console.log(`Listening in http://localhost:${appConfig.APP_PORT}`)
         })
